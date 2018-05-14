5a1238081d59dfc2c901f758686a999fc22c3cbc
viviantran27 <vvttran@ucdavis.edu>
2018-05-13 23:08:24 -0700

    match rpi script?
    need to double check/ "merge" changes with the script on the rpi

--- serial_datalogger.py
+++ serial_datalogger.py
@@ -2,26 +2,28 @@ import serial
 import time
 import csv
 
-ser = serial.Serial('COM4',9600)         #Update serial port
+ser = serial.Serial('/dev/ttyACM0',9600)         #Update serial port
 ser.flushInput()
 
 while True:
     try:
         #Reads and formats data from a single serial line into separate array elements
         ser_bytes = ser.readline()                                      
-        decoded_bytes=ser_bytes[0:len(ser_bytes)-2].decode("utf-8")     
+        decoded_bytes=ser_bytes.decode("utf-8")     
         decoded_bytes=decoded_bytes.split(',')                          
         
         #List of array elements expected in each line
-        latitude=float(decoded_bytes[0])
-        longitude=float(decoded_bytes[1])
-        windSpeed=float(decoded_bytes[2])
-        windDirection=flaot(decoded_bytes[3])
+        windDirection=float(decoded_bytes[0])
+        windSpeed=float(decoded_bytes[1])
+        latitude=float(decoded_bytes[2])
+        longitude=float(decoded_bytes[3])
+        boatSpeed=float(decoded_bytes[4])
+
         
         #Writes to .csv file
         with open("boatTracker.csv","a",newline='') as f:
             writer = csv.writer(f,delimiter=",")
-            writer.writerow([time.time(),latitude,longitude, windSpeed, windDirection])        
+            writer.writerow([time.time(),windDirection,windSpeed,latitude,longitude, boatSpeed])        
     except:
         print("Keyboard Interrupt")
         break
