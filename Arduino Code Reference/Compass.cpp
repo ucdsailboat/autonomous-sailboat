3f1e95ad5419f42ef2f482deb85ba568f978f515
Jordan Leung <jordanjj8@gmail.com>
2018-05-13 20:04:41 -0700

    Add files via upload

---
+++ Arduino Code Reference/Compass.cpp
@@ -0,0 +1,33 @@
+// Davi's code for the compass 
+// Updated by: JL 5/8/18
+// compass gives the boat the HEADING
+/* 
+Input: none
+Output: heading type float 
+*/
+
+float dir_bussola(){
+  float soma = 0;
+  for (int j = 1; j <= 10; j++){
+  Wire.beginTransmission(slaveAddress);
+  Wire.write("A");
+  Wire.endTransmission();
+  delay(10);
+  Wire.requestFrom(slaveAddress, 2);
+  int i = 0;
+  while(Wire.available() && i < 2)
+  { 
+    headingData[i] = Wire.read();
+    i++;
+  }
+  headingValue = headingData[0]*256 + headingData[1];
+  heading = headingValue/10;
+ //  heading = -heading;
+  heading = heading - norte_quadr_buss;
+  heading = saturador(heading);
+  soma += heading;
+  }
+
+ //  delay(100);
+  return soma/10;
+}
