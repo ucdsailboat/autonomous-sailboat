5d7c40297ff94e83a692ea18e24cdd4204ed6981
MicheleShi <35747281+MicheleShi@users.noreply.github.com>
2018-05-18 10:45:35 -0700

    Anemometer and Sail Controller with latest offsets

--- Arduino Hardware Code/Sail_Controller.ino
+++ Arduino Hardware Code/Sail_Controller.ino
@@ -1,192 +1,130 @@
-/* Sail Controller 
+/* Sail Controller - Modified 5/18 M.S. 
  *  Autonomous sail control using anemometer and servo
  *  Connections: 
- *  Joystick: VRx to A0,  VRy to A1, +5V to 5, and GND to GND
+ *  Anemometer: A4 for Wind Direction, Pin 2 for Wind Speed
  *  Sail Servo: Signal to Pin 7
   */
 
+// libraries 
 #include <math.h>  
 
-// assumed from rudder controller
-int h1Delta = 0;          // current heading
-int h2Delta = 0;          // previous heading 
+// Servo libraries 
+#include <Servo.h> 
+
+// assumed from rudder controller 
+int h1Delta = 0;          // current heading relative to boat (0 to 180 clockwise, 0 to -179 counterclockwise)
+int h2Delta = 0;          // previous heading relative to boat 
 
 // Anemometer Wind Direction Init  
-#define Offset 0;         // offset from true magnetic north if global heading desired 
 #define WindSensorPin (2) // The pin location of the anemometer sensor 
-int VaneValue;            // raw analog value from wind vane 
-int Direction;            // translated 0 - 360 direction 
-int CalDirection;         // converted value with offset applied z
+int VaneValue;            // raw analog value from wind vane
+int CalDirection;         // apparent wind direction: [0,180] clockwise, [0,-179] counterclockwise, 180 is dead zone
 
 // Anemometer Wind Speed Init 
-int Rotations;                            // displayed onto Serial Monitor 
-volatile unsigned long RotationsCounter;  // cup rotation counter used in interrupt routine 
-volatile unsigned long ContactBounceTime; // Timer to avoid contact bounce in interrupt routine 
-float WindSpeed;                          // speed knots 
+float WindSpeed;                          // apparent wind speed in knots 
 double T = 3.0;                           // time in seconds needed to average readings
 unsigned long WindSpeedInterval = T*1000; // time in milliseconds needed to average anemometer readings 
 unsigned long previousMillis = 0;         // millis() returns an unsigned long
-
-// Joystick Init
-int ledPin = 13;
-int joyPin1 = 0;            // slider variable connecetd to analog pin 0
-int joyPin2 = 1;            // slider variable connecetd to analog pin 1
-int x = 0;                  // variable to read the value from the analog pin 0
-int y = 0;                  // variable to read the value from the analog pin 1
-double offsetX;
-double offsetY; 
-int resolution = 20;
-double angle; 
+volatile unsigned long RotationsCounter;  // cup rotation counter used in interrupt routine 
+volatile unsigned long ContactBounceTime; // timer to avoid contact bounce in interrupt routine 
 
 // Sail Servo Init
-#include <Servo.h> 
-Servo servoS;               // creates servo object to control sail servo 
-int deg = 0;                // initialize input degree
-int sOffset = 40 - deg/12;  //degrees 
-int sLimit = 90;            // constraint angle limit to 90 for the sail 
-int sDelta;                 // nominal angle for sail servo 
+Servo servoS;                    // servo object for sail servo 
+int turnAngle = 45;              // turn angle in degrees to determine straight sailing or turns
+int sDesired = 0;                // desired sail angle relative to nose, uncalibrated  
+int sZero = 60;                  // degrees required to zero sail servo in line with nose of boat
+int sLimit = 90;                 // constraint angle limit to 90 degrees for the sail, determined by max slack allowed by rope length 
+int sOffset;                     // calculated offset from getSailOffset necessary to map servo commands to sail angle (0 to 90 relative to boat nose) 
+int sCommand;                    // calibrated angle command in degrees to servo.write 
+int spSail;                      // set point sail angle relative to wind direction 
 
 // Anemometer Wind Direction Prototyping
-void getHeading(int direction);       // defines ranges for compass rose
-
-// Joystick Prototying 
-int treatValue(int data);
+int getWindDirection(int VaneValue);           // faulty anemometer requires nonlinear equations for calibration
 
 // Sail Servo Prototyping
-int treatAngle(int data);
-int commandSailServo(int deg);
+int getSailOffset(int sDesired);               // 0.85*sDesired + 60; used with getSailServoCommand
+int getSailServoCommand(int sDesired);         // calibrate desired sail angle to angle command to servo.Write
 
 void setup() {
   Serial.begin(9600); // baud rate (the number of times a signal in a communications channel changes state)
-
+  
   // Anemometer Wind Direction Setup
   pinMode(WindSensorPin, INPUT); 
+  
+  // Anemometer Wind Speed Setup 
   attachInterrupt(digitalPinToInterrupt(WindSensorPin), isr_rotation, FALLING); 
   sei(); // Enables interrupts 
-  Serial.println("Vane Value\tDirection\tHeading\t\tRotations\tKnots\tSail Servo Angle"); 
-
-  // Joystick Setup
-  pinMode(ledPin, OUTPUT);              // initializes digital pins 0 to 7 as outputs
-  delay(75);
-  offsetY=analogRead(joyPin1);
-  offsetX=analogRead(joyPin2);  
 
   // Sail Servo Setup
-  servoS.attach(7);  //the pin for the servob control
-  servoS.write(sOffset);
+  servoS.attach(7);      // pin for the servoS control
+  servoS.write(sOffset); // initialize at zero position 
   
 }
-
 
 void loop() {
   // Anemometer Wind Direction Loop
   VaneValue = analogRead(A4); 
-  Direction = map(VaneValue, 0, 1023, 0, 360); 
-  CalDirection = Direction + Offset; 
+  CalDirection = getWindDirection(VaneValue);
 
-  if(CalDirection > 360) 
-  CalDirection = CalDirection - 360; 
-
-  if(CalDirection < 0) 
-  CalDirection = CalDirection + 360; 
- 
-  //Wind speed
+  // Curve fit outputs less than -179, so constrain to -179
+  if(CalDirection < -179) 
+  CalDirection = -179; 
+  
+  // Anemometer Wind Speed Loop 
   unsigned long currentMillis = millis(); // current run time
   if ((unsigned long)(currentMillis - previousMillis) >= WindSpeedInterval){  // calculate wind speed after 3 seconds 
     // convert to knots using the formula V = P(2.25/T) / 1.15078 
-    Rotations = RotationsCounter;                
     WindSpeed = RotationsCounter*2.25/T/1.15078;
-    RotationsCounter = 0;         // Set Rotations count to 0 ready for calculations 
+    RotationsCounter = 0;                 // reset RotationsCounter after averaging  
     previousMillis = millis();
     }
-    
-    Serial.print(VaneValue); Serial.print("\t\t");
-    Serial.print(CalDirection); Serial.print("\t\t"); 
-    getHeading(CalDirection); Serial.print("\t\t"); 
-    Serial.print(Rotations); Serial.print("\t\t"); 
-    Serial.println(WindSpeed); Serial.print("\t\t");
-
-  
-  // Joystick Loop
-  y = treatValue(round(analogRead(joyPin1)-offsetY));  
-  delay(25);    
-  x = treatValue(round(analogRead(joyPin2)-offsetX)); 
-  delay(25);
-  angle = round(atan2(x,y)*180/M_PI);   // convert x and y coordinates into angle in degrees
-  h1Delta = angle;          // use joystick as a heading input, comment out when using anemometer
   
   // Sail Servo Loop 
-  deg = treatAngle(h1Delta);  // convert heading input to sail angle between 0 and 90 degrees
-  if ((h2Delta - h1Delta) < 45){
-    int spDelta = 90;         // set point relative sail angle
-    sDelta = abs(CalDirection) - spDelta; 
+  if ((h2Delta - h1Delta) < turnAngle){           // if the trajectory turns the boat less than the turnAngle degrees, then maintain 90 degree relative sail angle
+    spSail = 90;                                  // set point sail angle relative to wind direction
+    sDesired = abs(abs(CalDirection) - spSail);   // CalDirection [-179,180], sail doesn't care about direction   
   }
   else {
-    sDelta = 90*abs(CalDirection)/180;
+    sDesired = sLimit*abs(CalDirection)/180;      // from Davi sail Control Law, executed on turns 
   }
-  commandSailServo(sDelta);
-  h2Delta = h1Delta;
-  }
-
-// Changes potentiometer values into +/- resolution 
-int treatValue(int data) {
-  return (data * (2*resolution+1) / 1024);
- }
-
-// constrain sail angle between 0 and 90 degrees 
-// input: data - heading angle (0 to 180 clockwise and 0 to -179 counterclockwise) 
-int treatAngle(int data){
-  if (data <= 180 && data >= 0) {
-  return data/4;
-  }
-  else {
-    return (data/4 + 90);
-  }
+  sCommand = getSailServoCommand(sDesired);       // calibrate desired sail angle to angle command for servo
+  servoS.write(sCommand);                         // command sail servo
+  h2Delta = h1Delta;                              // current heading becomes previous heading 
   }
 
-int commandSailServo(int deg){
-  if(deg >= sLimit) {
-    // note: 40 (deg) is the offset to zero
-    sOffset = 40 - deg/12;
-    Serial.println(sLimit); Serial.print("\t\t");
-    servoS.write(sLimit+sOffset);
+// Servo Function definitions
+int getSailOffset(int sDesired){            // used in getSailServoCommand function 
+  return sDesired*0.85 + sZero;             // curve fit of data 
+}
+
+int getSailServoCommand(int sDesired){
+  if(sDesired >= sLimit) {  // for desired sail angles greater than the limit, return the limit
+    sOffset = getSailOffset(sDesired);
+    return round(sLimit + sOffset);
     }
-   // runs if degrees are within the sLimit
-  else {
-    // note: 40 (deg) is the offset
-    sOffset = 40 - deg/12;
-    Serial.print("\t\t");
-    Serial.println(deg);
-    servoS.write(deg+sOffset);
+  else {                    // for all other sail angles, return the calibrated sail angle 
+    sOffset = getSailOffset(sDesired);
+    return round(sDesired + sOffset); 
     }
 }
 
 // Anemometer function definitions 
-// This is the function that the interrupt calls to increment the rotation count 
+// function that the interrupt calls to increment the rotation count 
 void isr_rotation () { 
-if ((millis() - ContactBounceTime) > 15 ) { // debounce the switch contact. 
-RotationsCounter++; 
-ContactBounceTime = millis(); 
-}
+if ((millis() - ContactBounceTime) > 15 ) { // debounce the switch contact.
+  RotationsCounter++; 
+  ContactBounceTime = millis(); 
+  }
+  }
+
+// potentiometer values mapped to [-179,180], curve fit from data
+int getWindDirection(int VaneValue){     
+  if (VaneValue <= 1023 && VaneValue > 683){  // [0,180] degrees
+    return round(0.5227*VaneValue - 354.42);  
+  }
+  else if (VaneValue < 683 && VaneValue >= 0){  // [-179,0] degrees
+    return round(0.0004*VaneValue*VaneValue - 0.0005*VaneValue - 188.4);
+  }
+  else return 0; 
 }
 
-void getHeading(int direction) { 
-if(direction < 22) 
-Serial.print("N"); 
-else if (direction < 67) 
-Serial.print("NE"); 
-else if (direction < 112) 
-Serial.print("E"); 
-else if (direction < 157) 
-Serial.print("SE"); 
-else if (direction < 212) 
-Serial.print("S"); 
-else if (direction < 247) 
-Serial.print("SW"); 
-else if (direction < 292) 
-Serial.print("W"); 
-else if (direction < 337) 
-Serial.print("NW"); 
-else 
-Serial.print("N"); 
-}
