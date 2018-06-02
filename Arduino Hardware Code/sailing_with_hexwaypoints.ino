b11ede107c421378108856b55ee8690b6bcd74d4
Jordan Leung <jordanjj8@gmail.com>
2018-06-02 12:53:21 -0700

    Update sailing_with_hexwaypoints.ino
    3-> 5

--- Arduino Hardware Code/sailing_with_hexwaypoints.ino
+++ Arduino Hardware Code/sailing_with_hexwaypoints.ino
@@ -53,7 +53,7 @@ int CalDirection;         // apparent wind direction: [0,180] CW, [0,-179] CCW, 
 // rudder ctrl 
 TinyGPS gps; // gps object
 Servo servoRudder; // declare a servo object
-float distMargin = 3.0; // margin of error for waypoints (defined as a radius in [meters]) 
+float distMargin = 5.0; // margin of error for waypoints (defined as a radius in [meters]) 
 // SoftwareSerial ss(TX, RX); // use pins 4 and 3 for SW serial ports
 //using hardware serial 
 //HardwareSerial mySerial = Serial1;
